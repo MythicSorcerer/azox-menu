375d92a3f4c3994c70549ee382e5120aede51ece
Mythic <ximo.tu@icloud.com>
2026-04-04 18:50:43 -0700

    Delay PacketEvents check to after all plugins load

--- src/main/java/net/azox/menu/listeners/ScoreboardListener.java
+++ src/main/java/net/azox/menu/listeners/ScoreboardListener.java
@@ -2,23 +2,30 @@ package net.azox.menu.listeners;
 
 import net.azox.menu.AzoxMenu;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.server.ServerLoadEvent;
+import org.bukkit.entity.Player;
 
 public class ScoreboardListener implements Listener {
 
     private final AzoxMenu plugin;
     private boolean packetEventsAvailable = false;
-    private boolean listenerRegistered = false;
+    private boolean checked = false;
 
     public ScoreboardListener(final AzoxMenu plugin) {
         this.plugin = plugin;
-        this.checkPacketEvents();
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            this.checkPacketEvents();
+        }, 20L);
     }
 
     private void checkPacketEvents() {
+        if (this.checked) {
+            return;
+        }
+        this.checked = true;
+        
         try {
             final Class<?> packetEventsClass = Class.forName("com.github.retrooper.packetevents.PacketEvents");
             final Object api = packetEventsClass.getMethod("getAPI").invoke(null);
@@ -30,11 +37,18 @@ public class ScoreboardListener implements Listener {
                 "registerListener", listenerClass
             );
             
+            final Class<?> wrapperClass = Class.forName(
+                "com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerScoreboardObjective"
+            );
+            final Class<?> scoreFormatClass = Class.forName(
+                "com.github.retrooper.packetevents.protocol.scoreboard.ScoreFormat"
+            );
+            final Class<?> eventClass = Class.forName("com.github.retrooper.packetevents.event.PacketSendEvent");
+            
             final java.lang.reflect.InvocationHandler handler = (proxy, method, args) -> {
                 if ("onPacketSend".equals(method.getName())) {
                     try {
                         final Object event = args[0];
-                        final Class<?> eventClass = event.getClass();
                         
                         final Class<?> packetTypeClass = Class.forName("com.github.retrooper.packetevents.protocol.packettype.PacketType");
                         final Object scoreboardType = packetTypeClass.getField("Play.Server.SCOREBOARD_OBJECTIVE").get(null);
@@ -42,19 +56,12 @@ public class ScoreboardListener implements Listener {
                         final Object eventPacketType = eventClass.getMethod("getPacketType").invoke(event);
                         
                         if (scoreboardType.equals(eventPacketType)) {
-                            final Class<?> wrapperClass = Class.forName(
-                                "com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerScoreboardObjective"
-                            );
                             final Object packet = wrapperClass.getConstructor(eventClass).newInstance(event);
                             
                             final String name = (String) wrapperClass.getMethod("getObjectiveName").invoke(packet);
                             
                             if ("azoxmenu".equals(name)) {
-                                final Class<?> scoreFormatClass = Class.forName(
-                                    "com.github.retrooper.packetevents.protocol.scoreboard.ScoreFormat"
-                                );
                                 final Object blankFormat = scoreFormatClass.getMethod("blankScore").invoke(null);
-                                
                                 wrapperClass.getMethod("setScoreFormat", scoreFormatClass).invoke(packet, blankFormat);
                                 eventClass.getMethod("markForReencode", boolean.class).invoke(event, true);
                             }
@@ -73,24 +80,12 @@ public class ScoreboardListener implements Listener {
             
             registerMethod.invoke(eventManager, listener);
             this.packetEventsAvailable = true;
-            this.listenerRegistered = true;
             this.plugin.getLogger().info("PacketEvents found - scoreboard numbers will be hidden.");
             
         } catch (final ClassNotFoundException e) {
             this.plugin.getLogger().warning("PacketEvents not found - scoreboard numbers will be visible.");
         } catch (final Exception e) {
-            this.plugin.getLogger().warning("Failed to initialize PacketEvents: " + e.getClass().getSimpleName() + " - " + e.getMessage());
+            this.plugin.getLogger().warning("Failed to initialize PacketEvents: " + e.getClass().getSimpleName());
         }
-    }
-
-    @EventHandler
-    public void onPlayerJoin(final PlayerJoinEvent event) {
-        if (!this.packetEventsAvailable || this.listenerRegistered) {
-            return;
-        }
-        
-        Bukkit.getScheduler().runTaskLater(this.plugin, () -> {
-            final ScoreboardListener listener = new ScoreboardListener(this.plugin);
-        }, 1L);
     }
 }
